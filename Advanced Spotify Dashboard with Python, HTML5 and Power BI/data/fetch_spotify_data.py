6b704aec7a876bc1d76b5d0efd5fafe072c3e4bf
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2023-11-10 16:00:31 +0100

    commit py-project

---
+++ Advanced Spotify Dashboard with Python, HTML5 and Power BI/data/fetch_spotify_data.py
@@ -0,0 +1,54 @@
+# Import required libraries
+import requests
+import pandas as pd
+
+# Initialize your client ID and client secret from Spotify Developer Dashboard
+CLIENT_ID = '5b2125b0646d4c29a36b3d50ad740475'
+CLIENT_SECRET = 'f225cc8ee87145f18b69774105dce323'
+
+# Fetch an access token from Spotify
+auth_response = requests.post(
+    'https://accounts.spotify.com/api/token',
+    data={'grant_type': 'client_credentials', 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET},
+)
+auth_data = auth_response.json()
+access_token = auth_data['access_token']
+
+# Prepare headers for API requests
+headers = {'Authorization': f'Bearer {access_token}'}
+
+# Load your existing CSV file into a DataFrame
+# Replace with the actual path to your file
+df = pd.read_csv('E:/DATA ANALYTICS WITH PYTHON/Spotify Dashboard with Python and Power BI/data/spotify-2023.csv', encoding='ISO-8859-1')
+
+# Create an empty list to store cover URLs
+cover_urls = []
+
+# Loop through each row in the DataFrame to search for tracks on Spotify
+for _, row in df.iterrows():
+    track_name = row['track_name']
+    artist_name = row['artist(s)_name']
+
+    query = f"track:{track_name} artist:{artist_name}"
+    search_response = requests.get(f"https://api.spotify.com/v1/search?q={query}&type=track", headers=headers)
+
+    search_data = search_response.json()
+
+    try:
+        if 'tracks' in search_data and 'items' in search_data['tracks'] and search_data['tracks']['items']:
+            cover_url = search_data['tracks']['items'][0]['album']['images'][0]['url']
+        else:
+            cover_url = 'Not Found'
+    except (KeyError, IndexError):
+        cover_url = 'Not Found'
+
+    cover_urls.append(cover_url)
+
+# Add the 'cover_url' column to the DataFrame
+df['cover_url'] = cover_urls
+
+# Save the updated DataFrame to a CSV file
+df.to_csv('updated_spotify_data.csv', index=False)
+
+# Confirm that the data has been saved
+print("Updated data has been saved to 'updated_spotify_data.csv'")
